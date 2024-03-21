c358950ae4899519a95f260fdfcd0a19282a1020
Bev7787 <eregister2me@gmail.com>
2024-03-21 16:52:31 +1100

    Add status command with placeholder functionality.

--- src/NoEscapeBot/cogs/query.py
+++ src/NoEscapeBot/cogs/query.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord.commands import SlashCommandGroup
 from discord import option
+import datetime
+import time
 
 class Query(commands.Cog):
 
@@ -22,8 +24,22 @@ class Query(commands.Cog):
 
     # pings the user which sent the command
     @test.command()
-    async def pinguser(self, ctx: discord.ApplicationContext):
-        await ctx.respond(f'<@!{ctx.author.id}>') 
+    async def status(self, ctx: discord.ApplicationContext):
+        embed = discord.Embed(
+            title="MQ Station Status",
+            description=f"Status as of <t:{int(time.mktime((datetime.datetime.now()).timetuple()))}>",
+            color=discord.Colour.brand_green() # Pycord provides a class with default colors you can choose from
+        )
+        embed.add_field(name="University Escalator Up", value="Status", inline=True)
+        embed.add_field(name="University Escalator Down", value="Status", inline=True)
+        embed.add_field(name="University Elevator", value="Status", inline=True)
+        embed.add_field(name="Centre Escalator Up", value="Status", inline=True)
+        embed.add_field(name="Centre Escalator Down", value="Status", inline=True)
+        embed.add_field(name="Centre Elevator", value="Status", inline=True)
+        embed.add_field(name="Concourse Escalator Up", value="Status", inline=True)
+        embed.add_field(name="Concourse Escalator Down", value="Status", inline=True)
+        embed.add_field(name="Concourse Elevator", value="Status", inline=True)
+        await ctx.respond(embed=embed) 
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Query(bot))
