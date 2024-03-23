f53627746340391b6aef91c4e5ae21efe4be33c1
Bev7787 <eregister2me@gmail.com>
2024-03-23 23:32:52 +1100

    Add locations dictionary for implementation of the verification function. Add 'green tick' reaction to the message that occurs with /respond. Change /respond response to Discord embed.

--- src/NoEscapeBot/cogs/query.py
+++ src/NoEscapeBot/cogs/query.py
@@ -4,6 +4,18 @@ from discord.commands import SlashCommandGroup
 from discord import option
 from datetime import datetime
 
+
+locations = {
+    'mquni-escalator-up': {'name': 'University Escalator Up', 'message': None},
+    'mquni-escalator-down': {'name': 'University Escalator Down', 'message': None},
+    'mquni-lift': {'name': 'University Lift', 'message': None},
+    'mqcentre-escalator-up': {'name': 'Centre Escalator Up', 'message': None},
+    'mqcentre-escalator-down': {'name': 'Centre Escalator Down', 'message': None},
+    'mqcentre-lift': {'name': 'Centre Lift', 'message': None},
+    'concourse-escalator-up': {'name': 'Platform Escalator Up', 'message': None},
+    'concourse-escalator-down': {'name': 'Platform Escalator Down', 'message': None},
+    'concourse-lift': {'name': 'Platform Lift', 'message': None}
+}
 def datetimeToDiscord(input_time):
     return int(input_time)
 
@@ -65,28 +77,39 @@ class Query(commands.Cog):
             description=f"Status as of <t:{getCurrTime()}>",
             color=discord.Colour.brand_green() # Pycord provides a class with default colors you can choose from
         )
-        statusUpdate(embed, "University Escalator Up", False)
-        statusUpdate(embed, "University Escalator Down", True)
-        statusUpdate(embed, "University Lift", True)
-        statusUpdate(embed, "Centre Escalator Up", True)
-        statusUpdate(embed, "Centre Escalator Down", True)
-        statusUpdate(embed, "Centre Lift", True)
-        statusUpdate(embed, "Platform Escalator Up", True)
-        statusUpdate(embed, "Platform Escalator Down", True)
-        statusUpdate(embed, "Platform Lift", True)
+        statusUpdate(embed, locations['mquni-escalator-up']['name'], False)
+        statusUpdate(embed, locations['mquni-escalator-down']['name'], True)
+        statusUpdate(embed, locations['mquni-lift']['name'], True)
+        statusUpdate(embed, locations['mqcentre-escalator-up']['name'], True)
+        statusUpdate(embed, locations['mqcentre-escalator-down']['name'], True)
+        statusUpdate(embed, locations['mqcentre-lift']['name'], True)
+        statusUpdate(embed, locations['concourse-escalator-up']['name'], True)
+        statusUpdate(embed, locations['concourse-escalator-down']['name'], True)
+        statusUpdate(embed, locations['concourse-lift']['name'], True)
         await ctx.respond(embed=embed) 
 
+    # send command, respond, delete after five seconds and add new message with status update with reaction
     @discord.command()
-    @option("location", description="Location and direction of travel to be updated.", required=True, choices=['University Escalator Up', 'University Escalator Down', 'University Lift', 
-                                                                                                                'Centre Escalator Up', 'Centre Escalator Down', 'Centre Lift', 
-                                                                                                                'Platform Escalator Up', 'Platform Escalator Down', 'Platform Lift'])
+    @option("location", description="Location and direction of travel to be updated.", required=True, choices=[locations['mquni-escalator-up']['name'], locations['mquni-escalator-down']['name'], locations['mquni-lift']['name'], 
+                                                                                                                locations['mqcentre-escalator-up']['name'], locations['mqcentre-escalator-down']['name'], locations['mqcentre-lift']['name'], 
+                                                                                                                locations['concourse-escalator-up']['name'], locations['concourse-escalator-down']['name'], locations['concourse-lift']['name']])
     @option("status", description="Working/Broken", required=True, choices=['working', 'broken'])
     async def update(self, 
                      ctx: discord.ApplicationContext, location, status):
-        is_working = True
-        if status.lower() == 'broken':
-            is_working = False
-        await ctx.respond(f'Setting status of {location}.\n{location} working is {is_working} as of <t:{getCurrTime()}>.')
+        try:
+            is_working = True
+            if status.lower() == 'broken':
+                is_working = False
+            embed = discord.Embed(
+                title=f'Status update for {location}',
+                description=f"{status.capitalize()} as of <t:{getCurrTime()}>",
+                color=discord.Colour.red() # Pycord provides a class with default colors you can choose from
+            )
+            await ctx.respond(f'Report received!', delete_after=2)
+            message = await ctx.channel.send(embed=embed)
+            await message.add_reaction('✅')
+        except ValueError:
+            await ctx.respond(f'This location already has been reported!', delete_after=2)
         
 
 def setup(bot): # this is called by Pycord to setup the cog
